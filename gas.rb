bcbe1b7969c5e481bdf1c2a9a55100e041771c90
rashdandekar@gmail.com <rashdandekar@gmail.com>
2019-08-04 19:03:03 +0800

    Separated main.rb

--- gas.rb
+++ gas.rb
@@ -53,17 +53,3 @@ class Gas
 end
 
 
-myinputs = Hash.new
-myinputs["name"] = "Dry Gas"
-myinputs["spgr"] = 0.699
-
-# myinputs.each do
-#     |k,v|
-#     print k
-#     puts " " << v.to_s
-# end
-
-mygas = Gas.new(myinputs)
-puts mygas.name
-puts "Specific gravity is " << mygas.spgr.to_s
-puts mygas.zfactor(3000.00, 180.00)
